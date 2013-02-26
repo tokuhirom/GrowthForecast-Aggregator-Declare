fe7d944707e33cc29ff9cf8b991b1990f6dff64e
tokuhirom <tokuhirom@livedoor.jp>
2013-02-26 19:30:26 +0900

    initial import

---
+++ lib/GrowthForecast/Client/Declare.pm
@@ -0,0 +1,39 @@
+package GrowthForecast::Client::Declare;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+GrowthForecast::Client::Declare - ...
+
+=head1 SYNOPSIS
+
+  use GrowthForecast::Client::Declare;
+
+=head1 DESCRIPTION
+
+GrowthForecast::Client::Declare is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
