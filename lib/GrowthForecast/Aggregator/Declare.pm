a2c8447ba167123efbd3d0b9fc7076ba7decc4e4
tokuhirom <tokuhirom@gmail.com>
2013-02-28 08:48:24 +0900

    docs

--- lib/GrowthForecast/Aggregator/Declare.pm
+++ lib/GrowthForecast/Aggregator/Declare.pm
@@ -71,10 +71,44 @@ GrowthForecast::Aggregator::Declare - Declarative interface for GrowthForecast c
             );
         };
     };
+    for my $query (@queries) {
+        $query->run(
+            dbh => $dbh,
+            ua  => $ua,
+            service => 'blog_service',
+            endpoint => 'http://exapmle.com/api/',
+        );
+    }
 
 =head1 DESCRIPTION
 
 GrowthForecast::Aggregator::Declare is a declarative client library for L<GrowthForecast>
+
+=head1 DSL
+
+=over 4
+
+=item gf { ... }
+
+This makes a scope to declare GrowthForecast metrics.
+
+This function returns list of Aggregators.
+
+=item section $name:Str, \&block
+
+    section 'member' => sub { ... };
+
+This function defines section. Under this function, db() and db_multi() function use the section name automatically.
+
+=item db(%args)
+
+Create L<GrowthForecast::Aggregator::DB> object using C<< %args >>.
+
+=item db_multi(%args)
+
+Create L<GrowthForecast::Aggregator::DBMulti> object using C<< %args >>.
+
+=back
 
 =head1 AUTHOR
 
